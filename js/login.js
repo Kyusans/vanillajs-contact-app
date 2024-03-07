4da03b746e24b61bf667270440c31ec492ef3c9d
Mel Macario <xmelmacario@gmail.com>
2024-03-08 05:30:24 +0800

    added signup.js

--- js/login.js
+++ js/login.js
@@ -1,3 +1,11 @@
+localStorage.setItem("url", "http://localhost/contact/api/");
+
 document.getElementById("login").addEventListener("click", () =>{
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
   
+  const jsonData = {
+    username: username,
+    password: password
+  }
 });
